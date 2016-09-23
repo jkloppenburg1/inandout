dde8fa2e212e39ef7762dbf88259c422bd677645
jkloppenburg1 <jkloppenburg1>
2016-09-22 17:25:46 -0700

    Created Order class

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/inandout/Order.java
@@ -0,0 +1,159 @@
+package edu.orangecoastcollege.cs273.inandout;
+
+/**
+ * Created by jkloppenburg1 on 9/22/2016.
+ */
+public class Order {
+    private int cheeseburgers;
+    private int doubleDoubles;
+    private int frenchFries;
+    private int shakes;
+    private int smallDrinks;
+    private int mediumDrinks;
+    private int largeDrinks;
+
+    private static final double PRICE_DOUBLE_DOUBLE = 3.60;
+    private static final double PRICE_CHEESEBURGER = 2.15;
+    private static final double PRICE_FRENCH_FRIES = 1.65;
+    private static final double PRICE_SHAKES = 2.20;
+    private static final double PRICE_SMALL_DRINK = 1.45;
+    private static final double PRICE_MEDIUM_DRINK = 1.55;
+    private static final double PRICE_LARGE_DRINK = 1.75;
+    private static final double TAX_RATE = 0.08;
+
+    public Order()
+    {
+        this.cheeseburgers = 0;
+        this.doubleDoubles = 0;
+        this.frenchFries = 0;
+        this.shakes = 0;
+        this.smallDrinks = 0;
+        this.mediumDrinks = 0;
+        this.largeDrinks = 0;
+    }
+
+    public Order(int cheeseburgers, int doubleDoubles, int frenchFries, int shakes, int smallDrinks, int mediumDrinks, int largeDrinks) {
+        this.cheeseburgers = cheeseburgers;
+        this.doubleDoubles = doubleDoubles;
+        this.frenchFries = frenchFries;
+        this.shakes = shakes;
+        this.smallDrinks = smallDrinks;
+        this.mediumDrinks = mediumDrinks;
+        this.largeDrinks = largeDrinks;
+    }
+
+    public double calculateSubtotal()
+    {
+        double total = 0.0;
+        total += this.cheeseburgers * PRICE_CHEESEBURGER;
+        total += this.doubleDoubles * PRICE_DOUBLE_DOUBLE;
+        total += this.frenchFries * PRICE_FRENCH_FRIES;
+        total += this.shakes * PRICE_SHAKES;
+        total += this.smallDrinks * PRICE_SMALL_DRINK;
+        total += this.mediumDrinks * PRICE_MEDIUM_DRINK;
+        total += this.largeDrinks * PRICE_LARGE_DRINK;
+
+        return total;
+    }
+
+    public double calculateTax()
+    {
+        return calculateSubtotal() * TAX_RATE;
+    }
+
+    public double  calculateTotal()
+    {
+        return calculateSubtotal() + calculateTax();
+    }
+
+
+    //Getters and setters
+    public int getCheeseburgers() {
+        return cheeseburgers;
+    }
+
+    public void setCheeseburgers(int cheeseburgers) {
+        this.cheeseburgers = cheeseburgers;
+    }
+
+    public int getDoubleDoubles() {
+        return doubleDoubles;
+    }
+
+    public void setDoubleDoubles(int doubleDoubles) {
+        this.doubleDoubles = doubleDoubles;
+    }
+
+    public int getFrenchFries() {
+        return frenchFries;
+    }
+
+    public void setFrenchFries(int frenchFries) {
+        this.frenchFries = frenchFries;
+    }
+
+    public int getShakes() {
+        return shakes;
+    }
+
+    public void setShakes(int shakes) {
+        this.shakes = shakes;
+    }
+
+    public int getSmallDrinks() {
+        return smallDrinks;
+    }
+
+    public void setSmallDrinks(int smallDrinks) {
+        this.smallDrinks = smallDrinks;
+    }
+
+    public int getMediumDrinks() {
+        return mediumDrinks;
+    }
+
+    public void setMediumDrinks(int mediumDrinks) {
+        this.mediumDrinks = mediumDrinks;
+    }
+
+    public int getLargeDrinks() {
+        return largeDrinks;
+    }
+
+    public void setLargeDrinks(int largeDrinks) {
+        this.largeDrinks = largeDrinks;
+    }
+
+    public static double getPriceDoubleDouble() {
+        return PRICE_DOUBLE_DOUBLE;
+    }
+
+    public static double getPriceCheeseburger() {
+        return PRICE_CHEESEBURGER;
+    }
+
+    public static double getPriceFrenchFries() {
+        return PRICE_FRENCH_FRIES;
+    }
+
+    public static double getPriceShakes() {
+        return PRICE_SHAKES;
+    }
+
+    public static double getPriceSmallDrink() {
+        return PRICE_SMALL_DRINK;
+    }
+
+    public static double getPriceMediumDrink() {
+        return PRICE_MEDIUM_DRINK;
+    }
+
+    public static double getPriceLargeDrink() {
+        return PRICE_LARGE_DRINK;
+    }
+
+    public static double getTaxRate() {
+        return TAX_RATE;
+    }
+
+}
--- app/src/main/java/edu/orangecoastcollege/cs273/inandout/SummaryActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/inandout/SummaryActivity.java
@@ -1,12 +1,49 @@
 package edu.orangecoastcollege.cs273.inandout;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SummaryActivity extends AppCompatActivity {
 
+    private TextView doubleDoubleTextView;
+    private TextView cheeseburgerTextView;
+    private TextView frenchFriesTextView;
+    private TextView shakesTextView;
+    private TextView smallDrinkTextView;
+    private TextView mediumDrinkTextView;
+    private TextView largeDrinkTextView;
+
+    private EditText doubleDoubleEditText;
+    private EditText cheeseburgerEditText;
+    private EditText frenchFriesEditText;
+    private EditText shakesEditText;
+    private EditText smallDrinkEditText;
+    private EditText mediumDrinkEditText;
+    private EditText largeDrinkEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        doubleDoubleTextView = (TextView) findViewById(R.id.doubleDoubleTextView);
+        cheeseburgerTextView = (TextView) findViewById(R.id.cheeseburgerTextView);
+        frenchFriesTextView = (TextView) findViewById(R.id.frenchFriesTextView);
+        shakesTextView = (TextView) findViewById(R.id.shakesTextView);
+        smallDrinkTextView = (TextView) findViewById(R.id.smallDrinkTextView);
+        mediumDrinkTextView = (TextView) findViewById(R.id.mediumDrinkTextView);
+        largeDrinkTextView = (TextView) findViewById(R.id.largeDrinkTextView);
+
+        doubleDoubleEditText = (EditText) findViewById(R.id.doubleDoubleEditText);
+        cheeseburgerEditText = (EditText) findViewById(R.id.cheeseburgerEditText);
+        frenchFriesEditText = (EditText) findViewById(R.id.frenchFriesEditText);
+        shakesEditText = (EditText) findViewById(R.id.shakesEditText);
+        smallDrinkEditText = (EditText) findViewById(R.id.smallDrinkEditText);
+        mediumDrinkEditText = (EditText) findViewById(R.id.mediumDrinkEditText);
+        largeDrinkEditText = (EditText) findViewById(R.id.largeDrinkEditText);
+
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
     }
